da7517545bfc9d8e432f803c5b040013d35c3196
teguh wahyudi <teguh.wahyu@gmail.com>
2021-09-22 00:27:32 +0700

    update interface

--- app/src/main/java/com/example/chapter4_challange/MainActivity.kt
+++ app/src/main/java/com/example/chapter4_challange/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageView
+import android.widget.TextView
 
 
 const val TAG = "MainActivity"
@@ -14,6 +15,14 @@ class MainActivity : AppCompatActivity() {
     private val imgUserRock: ImageView by lazy { findViewById(R.id.img_user_rock) }
     private val imgUserPaper: ImageView by lazy { findViewById(R.id.img_user_paper) }
     private val imgUserScissors: ImageView by lazy { findViewById(R.id.img_user_scissors) }
+
+    private val imgComRock: ImageView by lazy { findViewById(R.id.img_com_rock) }
+    private val imgComPaper: ImageView by lazy { findViewById(R.id.img_com_paper) }
+    private val imgComScissors: ImageView by lazy { findViewById(R.id.img_com_scissors) }
+
+    private val tvResult: TextView by lazy { findViewById(R.id.tv_result) }
+    private val imgRefresh: ImageView by lazy { findViewById(R.id.img_refresh) }
+
     private lateinit var userChoice: String
     private lateinit var comChoice: String
     private lateinit var result: String
@@ -36,6 +45,22 @@ class MainActivity : AppCompatActivity() {
             userChoice = getString(R.string.scissors)
             toggleUserHand(it as ImageView)
         }
+
+        imgRefresh.setOnClickListener {
+            for (hand in listOf(
+                imgUserPaper,
+                imgUserRock,
+                imgUserScissors,
+                imgComPaper,
+                imgComRock,
+                imgComScissors
+            )) {
+                hand.alpha = 1F
+                hand.setBackgroundColor(Color.WHITE)
+                tvResult.setText(R.string.vs)
+            }
+        }
+
     }
 
     private fun toggleUserHand(imgUser: ImageView) {
@@ -51,6 +76,18 @@ class MainActivity : AppCompatActivity() {
         getComputerHand()
     }
 
+    private fun drawComputerChoice() {
+        for (hand in listOf(imgComPaper, imgComRock, imgComScissors)) {
+            if (hand.tag != comChoice) {
+                hand.alpha = 0.6F
+                hand.setBackgroundColor(Color.WHITE)
+            } else {
+                hand.setBackgroundColor(Color.MAGENTA)
+                hand.alpha = 1F
+            }
+        }
+    }
+
     private fun getComputerHand() {
         comChoice = listOf(
             getString(R.string.rock),
@@ -58,6 +95,7 @@ class MainActivity : AppCompatActivity() {
             getString(R.string.scissors)
         ).random()
         Log.i(TAG, "Computer choose: $comChoice")
+        drawComputerChoice()
         getWinner()
     }
 
@@ -70,10 +108,13 @@ class MainActivity : AppCompatActivity() {
         val count = position[userChoice]!! - position[comChoice]!!
         if (userChoice == comChoice) {
             result = getString(R.string.draw)
+            tvResult.setText(R.string.draw)
         } else if ((count == 1) || (count == -2)) {
             result = getString(R.string.win)
+            tvResult.setText(R.string.win)
         } else {
             result = getString(R.string.lose)
+            tvResult.setText(R.string.lose)
         }
         Log.i(TAG, "Final result: $result")
     }
